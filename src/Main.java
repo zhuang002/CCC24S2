3d32cf72fe49601a371e4c2a2fd43d56a4ec630d
zhuang002 <zhuang001@gmail.com>
2024-04-10 11:47:27 -0400

    v2 smart

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -16,11 +17,22 @@ public class Main {
 	}
 
 	private static boolean strAltered(String line,int N) {
+		HashMap<Character, Boolean> heavyDic = new HashMap<>();
+		// build dictionary
+		for (int i=0;i<N;i++) {
+			char c = line.charAt(i);
+			if (heavyDic.containsKey(c)) {
+				heavyDic.put(c, true);
+			} else {
+				heavyDic.put(c, false);
+			}
+		}
+		
 		char c = line.charAt(0);
-		boolean lastHeavy = isHeavy(c, 0,  line, N);
+		boolean lastHeavy = heavyDic.get(c);
 		for (int i=1;i<N;i++) {
 			c = line.charAt(i);
-			boolean currentHeavy = isHeavy(c, i, line, N);
+			boolean currentHeavy = heavyDic.get(c);
 			if (lastHeavy == currentHeavy) {
 				return false;
 			}
@@ -29,14 +41,5 @@ public class Main {
 		return true;
 	}
 
-	private static boolean isHeavy(char c, int pos, String line, int n) {
-		// TODO Auto-generated method stub
-		for (int i=0;i<n;i++) {
-			if (i==pos) continue;
-			char c2 = line.charAt(i);
-			if (c == c2) return true;
-		}
-		return false;
-	}
-
+	
 }
